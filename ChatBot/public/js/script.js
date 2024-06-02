9f4bf3d719399c7e4d2e3009b790cea5438840ac
OhijMik <jihok148@gmail.com>
2024-06-02 15:30:59 -0400

    Added more responses
    - Added more responses

--- ChatBot/public/js/script.js
+++ ChatBot/public/js/script.js
@@ -7,8 +7,18 @@ var user = {message: ""};
 
 var arrayOfPossibleMessages = [
     {message:"hi", response:"hello"},
+    {message:"hello", response:"hi!"},
     {message:"how are you?", response:"I'm great!"},
-    {message:"what is your name?", response:"I am Chatbot!"}
+    {message:"how are you doing?", response:"I'm doing awesome!"},
+    {message:"what is your name?", response:"I am Chatbot!"},
+    {message:"good morning", response:"Good morning to you!"},
+    {message:"good evening", response:"Good evening to you!"},
+    {message:"good afternoon", response:"Good afternoon to you!"},
+    {message:"good night", response:"Good night!"},
+    {message:"thank you", response:"No problem!"},
+    {message:"goodbye", response:"Bye bye!"},
+    {message:"bye", response:"Bye!"},
+    {message:"are you a robot", response:"I am Chatbot!"},
 ];
 
 
@@ -27,7 +37,7 @@ function sendMessage(userMessage){
 function chatbotResponse(userMessage){
     var chatbotMessage = "";
 
-    if (userMessage.length > 5 || userMessage == "hi"){
+    if (userMessage.length > 1){
         var result = arrayOfPossibleMessages.filter(val => val.message.includes(userMessage.toLowerCase()));
     
         if (result.length > 0){
