8702bab154eb73b4a4895aee43799fc08f939135
OhijMik <jihok148@gmail.com>
2024-06-01 22:27:31 -0400

    Worked on the UI of the website
    - Made it so sending a text removes it from the text input box.
    - Sent messages are displayed at the top of the box.

--- ChatBot/public/js/script.js
+++ ChatBot/public/js/script.js
@@ -1,6 +1,32 @@
 
 var sendButton = document.getElementById('sendButton');
+var textbox = document.getElementById('textbox');
+var chatContainer = document.getElementById('chatContainer');
+
+var user = {message: ""};
+
+
+function sendMessage(userMessage){
+    var messageElement = document.createElement('div');
+    messageElement.style.textAlign = "right";
+    messageElement.style.margin = "10px";
+
+    messageElement.innerHTML = "<span> You: </span>"+
+                               "<span>" +userMessage+ "</span>";
+
+    chatContainer.appendChild(messageElement);
+}
+
 
 sendButton.addEventListener('click', function(e){
-    alert('button clicked');
+    var userMessage = textbox.value
+    
+    if (userMessage == ""){
+        alert("Please type a message");
+    } else{
+        let userMessageText = userMessage.trim();
+        user.message = userMessageText;
+        textbox.value = "";
+        sendMessage(userMessageText);
+    }
 })
